4f7d60a1ad9fd4e7916d54ae43a6a08090ccf25f
Lorenz <lorenz.weis@gmail.com>
2020-04-20 15:55:08 +0200

    initial remove

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function generate(id: string): string;
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function generate(id) {
-    return "hello" + id;
-}
-exports.generate = generate;
