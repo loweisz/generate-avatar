b6373a2e54519bfc024fa0d7b09984bd327698ec
Lorenz <lorenz.weis@gmail.com>
2020-04-21 17:12:10 +0200

    svg generate first nice picture + generate blob as return

--- example-app/src/index.js
+++ example-app/src/index.js
@@ -1,13 +1,9 @@
 import { generateFromUUID } from "../../lib/index";
 import { v4 as uuidv4 } from "uuid";
-const path = document.getElementById("genPath");
-const path2 = document.getElementById("genPath2");
-const container = document.getElementById("bg");
 
-const newObj = generateFromUUID(uuidv4());
-container.setAttribute("fill", `#${newObj.background}`);
-path.setAttribute("fill", `#${newObj.color}`);
-path2.setAttribute("fill", `#${newObj.color}`);
+const newBlob = generateFromUUID(uuidv4());
+const img = document.getElementById("image");
+const objectURL = URL.createObjectURL(newBlob);
+img.src = objectURL;
 
-path.setAttribute("d", newObj.path);
-path2.setAttribute("d", newObj.path);
+console.log(newBlob);
