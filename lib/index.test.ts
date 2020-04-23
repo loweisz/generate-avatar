eab8ae4d401133494387cd847e2a7a39712a262f
Lorenz <lorenz.weis@gmail.com>
2020-04-23 17:57:57 +0200

    fixed tests

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,5 +1,5 @@
-import { generateFromUUID } from "./index";
+import { generateFromString } from "./index";
 
 test("adds 1 + 2 to equal 3", () => {
-  console.log(generateFromUUID("e1699a7e-6b8e-44a6-9ba8-bc0e35736cda"));
+  console.log(generateFromString("e1699a7e-6b8e-44a6-9ba8-bc0e35736cda"));
 });
