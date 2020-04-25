c28bf36b1bbce393c88a5736f5b51bea699164af
Lorenz Weiß <lorenz.weis@gmail.com>
2020-04-25 13:10:48 +0200

    added tests

--- lib/generateFromString.ts
+++ lib/generateFromString.ts
@@ -6,43 +6,28 @@ interface UUIDSvgData {
   background: string;
 }
 
-function invertHex(hex: string): string {
+export function invertHex(hex: string): string {
   return (Number(`0x1${hex}`) ^ 0xffffff).toString(16).substr(1).toUpperCase();
 }
 
-function generatePath(curveVal: number, posVal: number) {
+export function generatePath(curveVal: number, posVal: number, index: number) {
   const cVal = curveVal;
   const bigC = 300 - cVal;
 
   const pos = posVal;
-  return `m 150 ${pos + 150}
-  Q ${bigC} ${cVal} ${pos + 150} 150 
-  Q ${bigC} ${bigC} 150 ${150 - pos} 
-  Q ${cVal} ${bigC} ${150 - pos} 150 
-  Q ${cVal} ${cVal} 150 ${pos + 150} 
-z 
-`;
+  return `m 150 ${100 + pos + 200 * index} Q ${bigC} ${cVal} ${
+    200 - pos - 200 * index
+  } 150 Q ${bigC} ${bigC} 150 ${200 - pos - 200 * index} Q ${cVal} ${bigC} ${
+    100 + pos + 200 * index
+  } 150 Q ${cVal} ${cVal} 150 ${100 + pos + 200 * index} z`;
 }
 
 function hydrateSVGBlog(data: UUIDSvgData): string {
-  const svgString = `
-    <svg
-      width="300"
-      height="300"
-      viewBox="0 0 300 300"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect id="bg" width="300" height="300" fill="#${data.background}" />
-      <path d="${data.paths[0]}" fill="#${data.colors[0]}" />
-      <path d="${data.paths[1]}" fill="#${data.colors[1]}" />
-      <path d="${data.paths[2]}" fill="#${data.colors[2]}" />
-    </svg>
-  `;
+  const svgString = `<svg width="300" height="300" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg"><rect id="bg" width="300" height="300" fill="#${data.background}" /><path d="${data.paths[0]}" fill="#${data.colors[0]}" /><path d="${data.paths[1]}" fill="#${data.colors[1]}" /><path d="${data.paths[2]}" fill="#${data.colors[2]}" /></svg>`;
   return svgString;
 }
 
 export function generateFromString(id: string): string {
-  console.log(md5(id));
   const idArr = md5(id)
     .split("")
     .filter((el) => el !== "-");
@@ -53,9 +38,9 @@ export function generateFromString(id: string): string {
   );
   const data = {
     paths: [
-      generatePath(arr[0], arr[1]),
-      generatePath(arr[2], arr[3]),
-      generatePath(arr[4], arr[5]),
+      generatePath(arr[0], arr[1], 2),
+      generatePath(arr[2], arr[3], 1),
+      generatePath(arr[4], arr[5], 0),
     ],
     colors: [elColor, invertHex(elColor), invertHex(backgroundColor)],
     background: backgroundColor,
--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,5 +1,56 @@
-import { generateFromString } from "./index";
+import {
+  generatePath,
+  generateFromString,
+  invertHex,
+} from "./generateFromString";
 
-test("adds 1 + 2 to equal 3", () => {
-  console.log(generateFromString("e1699a7e-6b8e-44a6-9ba8-bc0e35736cda"));
+describe("generatePath works", () => {
+  it("should generate with correct values", () => {
+    const expectedPath =
+      "m 150 522 Q 255 45 -222 150 Q 255 255 150 -222 Q 45 255 522 150 Q 45 45 150 522 z";
+    expect(generatePath(45, 22, 2)).toBe(expectedPath);
+  });
+  it("should generate with edge values", () => {
+    const expectedPath2 =
+      "m 150 755 Q 45 255 -455 150 Q 45 45 150 -455 Q 255 45 755 150 Q 255 255 150 755 z";
+    expect(generatePath(255, 255, 2)).toBe(expectedPath2);
+  });
+  it("should generate with zero values", () => {
+    const expectedPath2 =
+      "m 150 100 Q 300 0 200 150 Q 300 300 150 200 Q 0 300 100 150 Q 0 0 150 100 z";
+    expect(generatePath(0, 0, 0)).toBe(expectedPath2);
+  });
+});
+
+describe("generate from string works", () => {
+  it("should generate with correct values for simple string", () => {
+    const expectedSVG =
+      '<svg width="300" height="300" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg"><rect id="bg" width="300" height="300" fill="#098f6b" /><path d="m 150 570 Q 95 205 -270 150 Q 95 95 150 -270 Q 205 95 570 150 Q 205 205 150 570 z" fill="#27b4f6" /><path d="m 150 511 Q 267 33 -211 150 Q 267 267 150 -211 Q 33 267 511 150 Q 33 33 150 511 z" fill="#D84B09" /><path d="m 150 302 Q 185 115 -2 150 Q 185 185 150 -2 Q 115 185 302 150 Q 115 115 150 302 z" fill="#F67094" /></svg>';
+    expect(generateFromString("test")).toBe(expectedSVG);
+  });
+  it("should generate with correct values for uuid", () => {
+    const expectedSVG =
+      '<svg width="300" height="300" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg"><rect id="bg" width="300" height="300" fill="#e19eb6" /><path d="m 150 646 Q 293 7 -346 150 Q 293 293 150 -346 Q 7 293 646 150 Q 7 7 150 646 z" fill="#e84404" /><path d="m 150 399 Q 298 2 -99 150 Q 298 298 150 -99 Q 2 298 399 150 Q 2 2 150 399 z" fill="#17BBFB" /><path d="m 150 312 Q 271 29 -12 150 Q 271 271 150 -12 Q 29 271 312 150 Q 29 29 150 312 z" fill="#1E6149" /></svg>';
+    expect(generateFromString("438626b9-0d9a-4185-a1f5-3cf388740c74")).toBe(
+      expectedSVG
+    );
+  });
+});
+
+describe("invert hex works", () => {
+  it("should invert basic hex", () => {
+    const hexCol = "F56E24";
+    const invHexCol = "0A91DB";
+    expect(invertHex(hexCol)).toBe(invHexCol);
+  });
+  it("should invert black to white", () => {
+    const hexCol = "000000";
+    const invHexCol = "FFFFFF";
+    expect(invertHex(hexCol)).toBe(invHexCol);
+  });
+  it("should invert white to black", () => {
+    const hexCol = "FFFFFF";
+    const invHexCol = "000000";
+    expect(invertHex(hexCol)).toBe(invHexCol);
+  });
 });
